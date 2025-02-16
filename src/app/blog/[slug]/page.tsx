51d77c552b39e1d93f66bb2b922c9ec5383f0ad2
Aiden <rheech22@gmail.com>
2025-02-17 01:32:11 +0900

    feat: show title and caption for code

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,23 +4,34 @@ import { highlightCode } from "@/app/lib/highlight-code";
 type PreElementWithProps = React.ReactElement<{
   children?: string;
   className?: string;
+  "data-title"?: string;
+  "data-caption"?: string;
 }>;
 
 const parsePre = (pre: PreElementWithProps) => {
+  const title = pre.props["data-title"] ?? "";
+  const caption = pre.props["data-caption"] ?? "";
   const code = pre.props.children ?? "";
   const className = pre.props.className ?? "";
   const match = className.match(/language-(\w+)/);
   return {
     lang: match ? match[1] : "",
     code,
+    title,
+    caption,
   };
 };
 
 const Pre = async ({ children }: { children: PreElementWithProps }) => {
-  console.log(children);
   const pre = parsePre(children);
   const html = await highlightCode(pre.code, pre.lang);
-  return <code dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <div className="code-block">
+      {pre.title && <div className="code-block__title">{pre.title}</div>}
+      <code dangerouslySetInnerHTML={{ __html: html }} />
+      {pre.caption && <div className="code-block__caption">{pre.caption}</div>}
+    </div>
+  );
 };
 
 const components = {
