a22536670d2c106911017b86e336482390e7627d
Aiden <rheech22@gmail.com>
2025-03-09 23:44:01 +0900

    test: pre-commit

---
+++ src/app/references/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { MdxLayout } from "@/app/components/mdx-layout";
+import { getPosts, getSlug } from "@/app/lib/get-posts";
+
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const slug = (await params).slug;
+  const { default: Post } = await import(`../markdown/${slug}.mdx`);
+  return (
+    <MdxLayout>
+      <Post />
+    </MdxLayout>
+  );
+};
+
+export default Page;
+
+export const generateStaticParams = () => {
+  return getPosts().map((fileName) => ({ slug: getSlug(fileName) }));
+};
+
+export const dynamicParams = false;
