77ef68fd2ec97d537d49fc31e5839ca6b2fa4df2
Aiden <rheech22@gmail.com>
2025-03-17 05:19:46 +0900

    beta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div className="h-screen flex flex-col">
-      <main className="h-full flex">
-        <nav className="m-auto border-4 border-black flex flex-col items-center justify-center gap-12 text-4xl font-bold p-14">
-          <Link href="/blog/index" className="underline w-full text-center">
+      <main className="h-full flex flex-col">
+        <nav className="m-auto border-4 border-black flex flex-col items-center gap-12 text-4xl font-bold p-14">
+          <div className="text-4xl font-bold border-2 border-black">beta</div>
+          <Link
+            href="/blog/index"
+            className="underline w-full text-start hover:underline-offset-4"
+          >
             Blog
           </Link>
           <Link
             href="/fleeting-notes/index"
-            className="underline w-full text-center"
+            className="underline w-full text-end hover:underline-offset-4"
           >
             Fleeting Notes
           </Link>
           <Link
             href="/reference-notes/index"
-            className="underline w-full text-center"
+            className="underline w-full text-start hover:underline-offset-4"
           >
             Reference Notes
           </Link>
