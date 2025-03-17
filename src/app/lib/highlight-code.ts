5f314598170cfbf5217f34c004748d193f1721b5
Aiden <rheech22@gmail.com>
2025-03-17 17:31:08 +0900

    opti: shiki highlighter

--- src/app/lib/highlight-code.ts
+++ src/app/lib/highlight-code.ts
@@ -4,26 +4,13 @@ import {
   transformerNotationDiff,
   transformerNotationFocus,
 } from "@shikijs/transformers";
-import { createHighlighter } from "shiki";
 
-//const highlighterPromise = createHighlighterCore({
-//  themes: [
-//    import("@shikijs/themes/github-light-high-contrast"),
-//    // ..
-//  ],
-//  langs: [
-//    import("@shikijs/langs/typescript"),
-//    import("@shikijs/langs/javascript"),
-//    import("@shikijs/langs/jsx"),
-//    import("@shikijs/langs/tsx"),
-//    import("@shikijs/langs/dart"),
-//    import("@shikijs/langs/python"),
-//    import("@shikijs/langs/lua"),
-//    import("@shikijs/langs/shellscript"),
-//    // ..
-//  ],
-//  engine: createJavaScriptRegexEngine(),
-//});
+import {
+  type HighlighterGeneric,
+  type BundledLanguage,
+  type BundledTheme,
+  createHighlighter,
+} from "shiki";
 
 const highlighterPromise = createHighlighter({
   themes: [
@@ -43,25 +30,27 @@ const highlighterPromise = createHighlighter({
   ],
 });
 
-export async function highlightCode(code: string, lang: string) {
-  const highlighter = await highlighterPromise;
-  const html = highlighter.codeToHtml(code, {
-    lang,
-    theme: "github-light-high-contrast",
-    transformers: [
-      transformerNotationHighlight({
-        matchAlgorithm: "v3",
-      }),
-      transformerNotationWordHighlight({
-        matchAlgorithm: "v3",
-      }),
-      transformerNotationDiff({
-        matchAlgorithm: "v3",
-      }),
-      transformerNotationFocus({
-        matchAlgorithm: "v3",
-      }),
-    ],
-  });
-  return html;
-}
+const highlightBy =
+  (highlighter: HighlighterGeneric<BundledLanguage, BundledTheme>) =>
+  async (code: string, lang: string) => {
+    return highlighter.codeToHtml(code, {
+      lang,
+      theme: "github-light-high-contrast",
+      transformers: [
+        transformerNotationHighlight({
+          matchAlgorithm: "v3",
+        }),
+        transformerNotationWordHighlight({
+          matchAlgorithm: "v3",
+        }),
+        transformerNotationDiff({
+          matchAlgorithm: "v3",
+        }),
+        transformerNotationFocus({
+          matchAlgorithm: "v3",
+        }),
+      ],
+    });
+  };
+
+export const highlightCode = highlightBy(await highlighterPromise);
