da1f1854e6ecaa714c3bdce8b17d54fa53e25d49
Aiden <rheech22@gmail.com>
2025-04-28 01:47:10 +0900

    add note

--- src/app/components/image.tsx
+++ src/app/components/image.tsx
@@ -1,6 +1,8 @@
 import NextImage, { type ImageProps } from "next/image";
 
 export const Image: React.FC<ImageProps> = (props) => {
+  console.log(props);
+
   if (!(props.width && props.height)) {
     return <NextImage {...props} width={1280} height={720} />;
   }
