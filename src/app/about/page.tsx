8b3f328c6c571c6b32d98d48528aec9ec89993b3
Aiden <rheech22@gmail.com>
2026-07-04 16:42:05 +0900

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,11 @@ export default async function AboutPage() {
 
   return (
     <main className={cn('mx-auto flex w-full max-w-3xl flex-col gap-16 px-6 py-16')}>
+      <Section title={'안녕하세요'}>
+        <article className={cn('markdown text-lg mx-0')}>
+					2022년부터 개발자로 일하고 있습니다. 소프트웨어를 저를 표현하는 하나의 수단으로 여기고 있어요. 하나의 기술이나 분야에 갇히지 않고 관심이 가는 곳으로 향하려고 합니다. 현재 직장에서는 NextJS와 Flutter로 하이브리드 앱을 만들고 있어요.
+        </article>
+      </Section>
       <Section title={'this site'}>
         <article className={cn('markdown text-lg mx-0')}>
           <AboutContent />
